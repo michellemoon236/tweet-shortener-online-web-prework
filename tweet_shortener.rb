eb15ea6457ed550f1882507862962498160df48a
Michelle M <michellemoon236@gmail.com>
2019-03-24 00:13:18 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,47 @@
-# Write your code here.
+def dictionary 
+  dictionary = {
+  "hello" => 'hi',
+"to" => '2', 
+"two" => '2',
+"too" => '2',
+"for" => '4',
+"four" => '4',
+'be' => 'b',
+'you' => 'u',
+"at" => "@", 
+"and" => "&"
+}
+end
+
+def word_substituter (string)
+  array = string.split.map do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end
+  array.join(" ")
+end
+    
+def bulk_tweet_shortener (array)
+  array.map do |string|
+  puts word_substituter (string)
+end
+end
+
+def selective_tweet_shortener (string)
+  if string.length > 140
+     word_substituter (string)
+  else
+    string
+  end
+end
+
+def shortened_tweet_truncator (string)
+  if word_substituter(string).length > 140
+    word_substituter(string)[0...137]+("...")
+  else
+     word_substituter(string)
+  end
+end
