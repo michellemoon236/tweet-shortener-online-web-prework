4832304250843d020c6089aef921d6cb1a9898e4
Michelle M <michellemoon236@gmail.com>
2019-03-24 00:12:02 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,7 +40,7 @@ end
 
 def shortened_tweet_truncator (string)
   if word_substituter(string).length > 140
-    word_substituter(string)[0...140]+("...")
+    word_substituter(string)[0...137]+("...")
   else
      word_substituter(string)
   end
